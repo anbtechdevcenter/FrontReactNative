6eaf5cd42ea650b956738f37f474e276d7ac05c2
Jeong Hwan Seo <restnfeel@gmail.com>
2017-06-02 00:06:45 +0900

    modify

--- app/FrontReactNative.js
+++ app/FrontReactNative.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, StyleSheet } from 'react-native';
-import { Container, Content, List, ListItem, Text ,Form, Input ,Header, Left, Label, Item, Right,Body, Title, Button, Icon} from 'native-base';
+import { ActivityIndicator, StyleSheet , View} from 'react-native';
+import { Container, Content, List, ListItem, Text ,
+    Toast, Form, Input ,Header, Left, Label, Item, Right,Body, Title, Button, Icon} from 'native-base';
 import {AnbUtil} from './components';
 import RankView from './RankView';
 
@@ -17,7 +18,50 @@ class FrontReactNative extends Component {
       title : '에이앤비 그룹웨어'
     };
 
+    _handleLogin(){
+      //console.log("start is");
+      let data = {
+       "grant_type" : "password",
+       "username" : "jhseo@anbtech.com",
+       "password" : "1"
+       };
 
+      fetch("https://restnfeel.cloud.tyk.io/token/", {
+        method : 'post',
+        headers : {
+          'Accept': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization' : 'Basic YW5iZGV2Y2VudGVyLWNsaWVudC13aXRoLXNlY3JldDpkZXZjZW50ZXI='
+        },
+        body : JSON.stringify(data)
+      })
+      .then((response)=>response.json())
+      .then((responseData)=>{
+        //callfn();
+        console.log('token ', responseData);
+        let msg = responseData.error;
+        if(msg){
+          Toast.show({
+            //supportedOrientations="potrait",
+              text: msg,
+              position: 'center',
+              buttonText: 'Okay'
+          });
+        }
+
+
+      })
+      .catch((error)=>{
+        console.log("[error] ", error);
+        Toast.show({
+          //supportedOrientations="potrait",
+            text: error,
+            position: 'center',
+            buttonText: 'Okay'
+        });
+      });
+
+    }
 
     render() {
 
@@ -25,60 +69,34 @@ class FrontReactNative extends Component {
 
         return(
           <Container>
-
+            <View style={styles.container}>
             <Form>
                            <Item fixedLabel>
-                               <Label>Username</Label>
+                               <Label>Email</Label>
                                <Input value='jhseo@anbtech.com'/>
                            </Item>
                            <Item fixedLabel last>
                                <Label>Password</Label>
                                <Input value="1"/>
                            </Item>
-                           <Button>
-                             <Text onPress={()=>{
-                                 let data = {
-                                   'grant_type' : 'password',
-                       						'username' : 'jhseo@anbtech.com',
-                       						'password' : '1'
-                                };
-                               fetch("https://restnfeel.cloud.tyk.io/oauth/token/", {
-                                 method : 'post',
-                                 headers : {
-                                   'Accept': 'application/json',
-                                   'Content-Type': 'application/x-www-form-urlencoded',
-                                   'Authorization' : 'Basic YW5iZGV2Y2VudGVyLWNsaWVudC13aXRoLXNlY3JldDpkZXZjZW50ZXI= '
-                                 },
-                                 body : JSON.stringify(data)
-                               })
-                               .then((response)=>{
-                                 //callfn();
-                                 console.log('token ', response);
-                               });
-
-                               }}>Login</Text>
+                           <Button full rounded>
+                             <Text onPress={this._handleLogin}>Login</Text>
                              </Button>
                        </Form>
 
-            <Text>메인화면이며 현재는 각 화면 확인용 진입점</Text>
-
-            <Button full
-              onPress={()=>navigate('Rank')}>
-              <Text>직급보기</Text>
-            </Button>
-            <Button full success
-              onPress={()=>navigate('CodeType')}>
-              <Text>코드타입관리</Text>
-            </Button>
-            <Button full info
-              onPress={()=>navigate('Staff')}>
-              <Text>직원보기</Text>
-            </Button>
-
+</View>
           </Container>
         );
     }
 }
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 2,
+    justifyContent : 'center',
+  //  alignItems : 'center'
+  }
+});
 
 
 export default FrontReactNative;
